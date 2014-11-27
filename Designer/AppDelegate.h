135ce19bc3b23805f6a5b317b9ecbf077b820937
xianyang <1029026512@qq.com>
2014-11-27 20:35:19 +0800

    Initial Commit

---
+++ Designer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Designer
+//
+//  Created by 罗 显扬 on 11/27/14.
+//  Copyright (c) 2014 罗 显扬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Designer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Designer
+//
+//  Created by 罗 显扬 on 11/27/14.
+//  Copyright (c) 2014 罗 显扬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
